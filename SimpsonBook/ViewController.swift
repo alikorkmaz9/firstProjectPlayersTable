35c0e8f8664c0c7b74a2130c61a37bee4d7cf57e
“alikorkmaz9” <alisebibkorkmaz@hotmail.com>
2020-11-21 14:52:04 +0300

    Initial Commit

---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by RIDVAN on 21.11.2020.
+//  Copyright © 2020 ridvanmertoglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
